601ef01dda7b18c724c58888b59301990709d8ba
sbouhussein <79649971+sbouhussein@users.noreply.github.com>
2022-04-07 15:59:12 -0400

    Buttons and Layouts work now

--- SeeFoodApp/app/src/main/java/com/example/seefoodapp/MyDietActivity.kt
+++ SeeFoodApp/app/src/main/java/com/example/seefoodapp/MyDietActivity.kt
@@ -1,11 +1,17 @@
 package com.example.seefoodapp
 
+import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MyDietActivity {
+class MyDietActivity : AppCompatActivity() {
 
     private lateinit var scanLabelCamera: Button
     private lateinit var scanLabelImage: Button
     private lateinit var clearDiet: Button
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_diet_activity)
+    }
 }
--- SeeFoodApp/app/src/main/java/com/example/seefoodapp/NewDietActivity.kt
+++ SeeFoodApp/app/src/main/java/com/example/seefoodapp/NewDietActivity.kt
@@ -1,9 +1,16 @@
 package com.example.seefoodapp
 
+import android.os.Bundle
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-class NewDietActivity {
+class NewDietActivity : AppCompatActivity() {
 
     private lateinit var dietName: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_diet_activity)
+    }
 
 }
