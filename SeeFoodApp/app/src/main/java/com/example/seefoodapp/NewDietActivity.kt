ba98060f4e0485b5538821457c0a285b6d3c8841
Arina Golikova <agolikov@umd.edu>
2022-04-15 18:50:03 -0400

    finished other 2 xml

--- SeeFoodApp/app/src/main/java/com/example/seefoodapp/NewDietActivity.kt
+++ SeeFoodApp/app/src/main/java/com/example/seefoodapp/NewDietActivity.kt
@@ -1,16 +1,33 @@
 package com.example.seefoodapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class NewDietActivity : AppCompatActivity() {
 
-    private lateinit var dietName: EditText
+    private lateinit var mName: EditText
+    private lateinit var mWeight: EditText
+    private lateinit var mHeight: EditText
+    private lateinit var saveButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_diet_activity)
+
+        saveButton = findViewById<Button>(R.id.saveBtn)
+        mName = findViewById<EditText>(R.id.nameText)
+        mWeight = findViewById<EditText>(R.id.weightText)
+        mHeight = findViewById<EditText>(R.id.heightText)
+
+
+        val myDietIntent = Intent(this, MyDietActivity::class.java)
+
+        saveButton.setOnClickListener() {
+            startActivity(myDietIntent)
+        }
     }
 
 }
